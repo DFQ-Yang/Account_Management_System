178bc7b9d8a19724f2ef0eb50043eb16c66b098d
DFQ-Yang <zirui522@gmail.com>
2025-12-18 19:25:47 -0800

    feat: print helloWorld with JavaEE framework

---
+++ src/main/java/com/dimo/account_management_system_javaee/helloWorld.java
@@ -0,0 +1,34 @@
+package com.dimo.account_management_system_javaee;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/helloWorld")
+public class helloWorld implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("helloWorld");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
